f971ef19bb2247c078e46f383908e287d4b6e94e
LeCongAnh212 <anhlc_intern@flydinotech.com>
2024-04-23 11:28:20 +0700

    add feature update info client

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\ClientRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Services\User\UpdateUserService;
 
 class UserController extends Controller
 {
-    
+
+    public function update(UpdateUserRequest $request)
+    {
+        $check = resolve(UpdateUserService::class)->setParams($request->all())->handle();
+
+        if ($check) {
+            return $this->responseSuccess(__('messages.update_success'));
+        }
+
+        return $this->responseErrors(__('messages.error_server'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'user'], function () {
+    Route::put('/update', [UserController::class, 'update']);
 });
